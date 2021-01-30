9f235a1bf81aa15d67327ca4488d3b9407f00744
maulanarifai114 <maulanarifai114@gmail.com>
2021-01-30 07:47:44 +0700

    modular at router index

---
+++ src/router/auth.js
@@ -0,0 +1,53 @@
+// Auth
+import MainAuth from '../views/auth/main/MainAuth.vue'
+import Login from '../views/auth/login/Login.vue'
+import SignUp from '../views/auth/signup/SignUp.vue'
+import Forgot from '../views/auth/forgotpass/ForgotPass.vue'
+import CreateNewPass from '../views/auth/forgotpass/CreateNewPass.vue'
+import CreatePin from '../views/auth/signup/CreatePin.vue'
+import CreatePinSuccess from '../views/auth/signup/CreatePinSuccess.vue'
+
+export default {
+  path: '/auth',
+  name: 'MainAuth',
+  component: MainAuth,
+  redirect: '/auth/login',
+  children: [
+    {
+      path: 'login',
+      name: 'Login',
+      component: Login
+      // meta: { requiresVisitor: true }
+    },
+    {
+      path: 'signup',
+      name: 'SignUp',
+      component: SignUp
+      // meta: { requiresVisitor: true }
+    },
+    {
+      path: 'forgot',
+      name: 'Forgot',
+      component: Forgot
+      // meta: { requiresVisitor: true }
+    },
+    {
+      path: 'createnewpass/:token',
+      name: 'CreateNewPass',
+      component: CreateNewPass
+      // meta: { requiresVisitor: true }
+    },
+    {
+      path: 'createpin/:token',
+      name: 'CreatePin',
+      component: CreatePin
+      // meta: { requiresVisitor: true }
+    },
+    {
+      path: 'createpinsuccess',
+      name: 'CreatePinSuccess',
+      component: CreatePinSuccess
+      // meta: { requiresVisitor: true }
+    }
+  ]
+}
--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,8 @@ import VueRouter from 'vue-router'
 
 // Landing Page
 import Landing from '../views/landing/Landing.vue'
-
 // Auth
-import MainAuth from '../views/auth/main/MainAuth.vue'
-import Login from '../views/auth/login/Login.vue'
-import SignUp from '../views/auth/signup/SignUp.vue'
-import Forgot from '../views/auth/forgotpass/ForgotPass.vue'
-import CreateNewPass from '../views/auth/forgotpass/CreateNewPass.vue'
-import CreatePin from '../views/auth/signup/CreatePin.vue'
-import CreatePinSuccess from '../views/auth/signup/CreatePinSuccess.vue'
+import Auth from './auth'
 
 Vue.use(VueRouter)
 
@@ -21,51 +14,7 @@ const routes = [
     name: 'Landing',
     component: Landing
   },
-  {
-    path: '/auth',
-    name: 'MainAuth',
-    component: MainAuth,
-    redirect: '/auth/login',
-    children: [
-      {
-        path: 'login',
-        name: 'Login',
-        component: Login
-        // meta: { requiresVisitor: true }
-      },
-      {
-        path: 'signup',
-        name: 'SignUp',
-        component: SignUp
-        // meta: { requiresVisitor: true }
-      },
-      {
-        path: 'forgot',
-        name: 'Forgot',
-        component: Forgot
-        // meta: { requiresVisitor: true }
-      },
-      {
-        path: 'createnewpass/:token',
-        name: 'CreateNewPass',
-        component: CreateNewPass
-        // meta: { requiresVisitor: true }
-      },
-      {
-        path: 'createpin/:token',
-        name: 'CreatePin',
-        component: CreatePin
-        // meta: { requiresVisitor: true }
-      },
-      {
-        path: 'createpinsuccess',
-        name: 'CreatePinSuccess',
-        component: CreatePinSuccess
-        // meta: { requiresVisitor: true }
-      }
-    ]
-  }
-
+  Auth
 ]
 
 const router = new VueRouter({
